d75a35ce991ce72a986a70e204e0ae0df8a4371c
Eduardo guadalupe Romero quiñonez <eduardogrq@Eduardos-iMac.local>
2021-07-23 02:10:54 -0500

    Se agrega video, se vinculan de manera estática los links de materias y recursos de muestra

--- js/script.js
+++ js/script.js
@@ -10,12 +10,22 @@ menu.addEventListener('click', toggleMenu, false);
 
 // Primer Grado********** Primer Grado********** Primer Grado********** Primer Grado********** Primer Grado********** Primer Grado**********
 
+let hideResourses = () =>{
+        document.querySelectorAll('.school-resources').forEach(element => {
+                element.classList.add('d-none')
+                element.classList.remove('d-block')
+        })
+}
+
 // Desplegar materias al hacer click en el grado
+
+
 
 let subjects = $('#first').click(function(){
         $('#segundo-grado').removeClass('d-block').addClass('d-none');
         $('#tercer-grado').removeClass('d-block').addClass('d-none');
 
+        hideResourses()
         $('#primer-grado').addClass('d-block');
 })
 
@@ -24,6 +34,7 @@ $('#second').click(function(){
         $('#primer-grado').removeClass('d-block').addClass('d-none');
         $('#tercer-grado').removeClass('d-block').addClass('d-none');
 
+        hideResourses()
         $('#segundo-grado').addClass('d-block');
 })
 
@@ -31,63 +42,54 @@ $('#third').click(function(){
         $('#primer-grado').removeClass('d-block').addClass('d-none');
         $('#segundo-grado').removeClass('d-block').addClass('d-none');
 
+        hideResourses()
         $('#tercer-grado').addClass('d-block');
 })
 
+// Recursos ****************
+
+
+
 // Recursos de Biología
 $('#biologia-materia').click(function(){
-        document.querySelectorAll('.school-resources').forEach(element => {
-                element.classList.add('d-none')
-                element.classList.remove('d-block')
-        })
+
+        hideResourses()
         $('#biologia-recursos').addClass(' d-block');        
 })
 // Recursos de Español
 $('#espanol-materia').click(function(){
-        document.querySelectorAll('.school-resources').forEach(element => {
-                element.classList.add('d-none')
-                element.classList.remove('d-block')
-        })
+
+        hideResourses()
         $('#espanol-recursos').addClass(' d-block');       
 })
 // Recursos de FCE
 $('#fce-materia').click(function(){
-        document.querySelectorAll('.school-resources').forEach(element => {
-                element.classList.add('d-none')
-                element.classList.remove('d-block')
-        })
+
+        hideResourses()
         $('#fce-recursos').addClass(' d-block');       
 })
 
 // Recursos de Geografía
 $('#geografia-materia').click(function(){
-        document.querySelectorAll('.school-resources').forEach(element => {
-                element.classList.add('d-none')
-                element.classList.remove('d-block')
-        })
+        
+        hideResourses()
         $('#geografia-recursos').addClass(' d-block');       
 })
 // Recursos de FCE
 $('#historia-materia').click(function(){
-        document.querySelectorAll('.school-resources').forEach(element => {
-                element.classList.add('d-none')
-                element.classList.remove('d-block')
-        })
+        
+        hideResourses()
         $('#historia-recursos').addClass(' d-block');       
 })
 // Recursos de FCE
 $('#ingles-materia').click(function(){
-        document.querySelectorAll('.school-resources').forEach(element => {
-                element.classList.add('d-none')
-                element.classList.remove('d-block')
-        })
+        
+        hideResourses()
         $('#ingles-recursos').addClass(' d-block');       
 })
 // Recursos de FCE
 $('#matematicas-materia').click(function(){
-        document.querySelectorAll('.school-resources').forEach(element => {
-                element.classList.add('d-none')
-                element.classList.remove('d-block')
-        })
+        
+        hideResourses()
         $('#matematicas-recursos').addClass(' d-block');       
 })
