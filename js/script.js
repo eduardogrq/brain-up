ea560a1201d194078ea213b5ddede3925250c65f
Eduardo guadalupe Romero quiñonez <eduardogrq@Eduardos-iMac.local>
2021-07-26 19:11:49 -0500

    Se crean vistas de videos para redireccionar al home, se condiciona dependiendo de qué video vengan para mostrar seleccionados el grado y los recursos del primer libro

--- js/script.js
+++ js/script.js
@@ -15,6 +15,7 @@ let hideResourses = () =>{
                 element.classList.add('d-none')
                 element.classList.remove('d-block')
         })
+
 }
 
 // Desplegar materias al hacer click en el grado
@@ -48,7 +49,7 @@ $('#third').click(function(){
 
 // Recursos ****************
 
-
+// Primer grado ************************
 
 // Recursos de Biología
 $('#biologia-materia').click(function(){
@@ -75,21 +76,80 @@ $('#geografia-materia').click(function(){
         hideResourses()
         $('#geografia-recursos').addClass(' d-block');       
 })
-// Recursos de FCE
+// Recursos de historia
 $('#historia-materia').click(function(){
         
         hideResourses()
         $('#historia-recursos').addClass(' d-block');       
 })
-// Recursos de FCE
+// Recursos de inglés
 $('#ingles-materia').click(function(){
         
         hideResourses()
         $('#ingles-recursos').addClass(' d-block');       
 })
-// Recursos de FCE
+// Recursos de Matemáticas
 $('#matematicas-materia').click(function(){
         
         hideResourses()
         $('#matematicas-recursos').addClass(' d-block');       
 })
+
+// Segundo grado ************************
+
+$('#segundo-espanol-materia').click(function(){
+   
+        hideResourses()
+        $('#segundo-espanol-recursos').addClass(' d-block');      
+})
+
+$('#segundo-fce-materia').click(function(){
+   
+        hideResourses()
+        $('#segundo-fce-recursos').addClass(' d-block');      
+})
+
+
+
+
+// Videos ***************************************************************
+
+// Cambiamos counter para poder condicionar en index que se muestre cada elemento
+function first(){
+        localStorage.setItem("counter", "1")
+}
+
+function second(){
+        localStorage.setItem("counter", "2")
+
+}
+
+function third(){
+        localStorage.setItem("counter", "3")
+
+}
+
+// console.log(localStorage.getItem("counter"))
+
+let counter = localStorage.getItem("counter")
+
+if(localStorage.getItem("counter") == 1){
+        $('#primer-grado').removeClass('d-none').addClass('d-block')
+        
+        document.getElementById('respuesta-1').checked = true;
+        document.getElementById('primero-biologia').checked = true;
+} else if(localStorage.getItem("counter") == 2){
+        $('#primer-grado').removeClass('d-block').addClass('d-none')
+        $('#biologia-recursos').removeClass('d-block').addClass('d-none')
+
+        $('#segundo-espanol-recursos').removeClass('d-none').addClass('d-block')
+        $('#segundo-grado').removeClass('d-none').addClass('d-block')
+
+        document.getElementById('respuesta-2').checked = true;
+        document.getElementById('segundo-espanol').checked = true;
+} else if(localStorage.getItem("counter") == 3){
+        
+
+        document.getElementById('respuesta-3').checked = true;
+        document.getElementById('tercero-espanol').checked = true;
+}
